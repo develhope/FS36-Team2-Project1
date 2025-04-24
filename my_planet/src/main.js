86b4e124657582443a54d88e79666a8fe369974d
sandro997 <alessandrofavazza97@gmail.com>
2025-04-24 16:16:41 +0200

    made directionalLight

--- my_planet/src/main.js
+++ my_planet/src/main.js
@@ -1,8 +1,6 @@
 import "./style.css";
 
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
-import { add, array } from "three/tsl";
 
 const scene = new THREE.Scene();
 
@@ -17,48 +15,50 @@ const render = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
 });
 
+//camera
+
 render.setPixelRatio(window.devicePixelRatio);
 render.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-render.render(scene, camera);
+//planet
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const textureLoader = new THREE.TextureLoader();
+const earthTexture = textureLoader.load("./night_earth.jpeg");
+const geometry = new THREE.SphereGeometry(10, 27, 20);
+const material = new THREE.MeshStandardMaterial({ map: earthTexture });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(10, 10, 10);
+//light
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(15, 15, 15);
+directionalLight.castShadow = true;
+scene.add(directionalLight);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper);
+//stars
 
-// function addStar() {
-//   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-//   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-//   const star = new THREE.Mesh(geometry, material);
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
 
-//   const [x, y, z] = array(3)
-//     .fill()
-//     .map(() => THREE.MathUtils.randFloatSpread(100));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-//   star.position.set(x, y, z);
-//   scene.add(star);
-// }
+  star.position.set(x, y, z);
+  scene.add(star);
+}
 
-// Array(100).fill().forEach(addStar);
+Array(100).fill().forEach(addStar);
 
-const controls = new OrbitControls(camera, render.domElement);
+//animation
 
 function animate() {
   requestAnimationFrame(animate);
   sphere.rotation.y -= 0.001;
-
-  controls.update();
 
   render.render(scene, camera);
 }
