5d73808e89358a8cf9440b4068f3539d4d74b7da
klevi0212 <klevi.smula@gmail.com>
2025-04-29 09:48:42 +0200

    swiper funziona

--- section-third-swiper.js
+++ section-third-swiper.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const swiper = new Swiper(".swiper", {
     effect: "coverflow",
-    centeredSlides: true,
     speed: 600,
     allowTouchMove: false,
-    slidesPerView: "auto",
+    centeredSlides: true,
+    slidesPerView: 3,
     initialSlide: 3,
+    loop: true,
     coverflowEffect: {
       rotate: 0,
       stretch: 80,
@@ -13,9 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
       modifier: 1,
       slideShadow: true,
     },
+    // slideActiveClass: "pippo",
     autoplay: {
-      delay: 1000,
+      delay: 3000,
       disableOnInteraction: false,
     },
+  });
+  swiper.on("slideChange", function () {
+    console.log(swiper);
+
+    // const nextSlide = document.querySelector(
+    //   `[data-swiper-slide-index="${swiper.activeIndex + 1}"]`
+    // );
+    // nextSlide.classList.add("hidden");
   });
 });
