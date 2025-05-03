7795023e4b91d623d6b2aca90a17b5b88df4c973
sandro997 <alessandrofavazza97@gmail.com>
2025-05-03 11:12:57 +0200

    added all sections

--- JS/footer.js
+++ JS/footer.js
@@ -2,25 +2,52 @@ const steps = document.querySelectorAll(".sales-steps li");
 const leftImg = document.querySelector(".img-left");
 const rightImg = document.querySelector(".img-right");
 
-const images = {
+const footerImages = {
   1: [
-    "foto_video/sales/36138f611ff7a9bb25d679290f623a99.jpg",
-    "foto_video/sales/669022e522248e4ab88aeeb1165486d0.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p1_first.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p1_second.jpg",
   ],
   2: [
-    "foto_video/sales/890dbeb471a10637528a72179b12bfa9.jpg",
-    "foto_video/sales/742068a6e1f7a94ba0ee51e40bea7226.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p2_first.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p2_second.jpg",
   ],
   3: [
-    "foto_video/sales/bcb8bcb289a7335a482b0769b77ab421.jpg",
-    "foto_video/sales/ce0048c94b712ae773a1f6371ced6303.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p3_first.jpg",
+    "./foto/foto_video/inizia_a_vendere_in_un_battibaleno/p3_second.jpg",
   ],
 };
 
-steps.forEach((step) => {
+const originalImages = [
+  "./foto_video/inizia_a_vendere_in_un_battibaleno/original_left.jpg",
+  "./foto_video/inizia_a_vendere_in_un_battibaleno/original_right.jpg",
+];
+
+// steps.forEach((step) => {
+//   step.addEventListener("mouseenter", () => {
+//     const stepNum = step.dataset.step;
+//     leftImg.src = images[stepNum][0];
+//     rightImg.src = images[stepNum][1];
+//   });
+// });
+
+function mouseOver(step) {
   step.addEventListener("mouseenter", () => {
-    const stepNum = step.dataset.step;
-    leftImg.src = images[stepNum][0];
-    rightImg.src = images[stepNum][1];
+    const stepKey = step.dataset.step;
+    const stepImages = images[stepKey];
+
+    if (stepImages) {
+      leftImg.src = stepImages[0];
+      rightImg.src = stepImages[1];
+    } else {
+      console.warn("Nessuna immagine trovata per step:", stepKey);
+    }
   });
+
+  step.addEventListener("mouseleave", () => {
+    leftImg.src = originalImages[0];
+    rightImg.src = originalImages[1];
+  });
+}
+steps.forEach((step) => {
+  mouseOver(step);
 });
